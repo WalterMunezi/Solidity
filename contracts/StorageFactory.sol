c997f986f9655e9199491edfd1061dfd7624988b
WalterMunezi <waltermunezi@gmail.com>
2024-02-14 13:31:36 +0200

    new files

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./SimpleStorage.sol";
 
 contract StorageFactory{
-    // i love git
-    function addnew()public pure returns(uint256){
-        uint256 add;
-        return add;
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public{
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
+
+    //Accessing functions from the simple storage contract
+    function sfStore(uint _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //to do this we need the address as well as the Application binary interface
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
+    }
+
 }
